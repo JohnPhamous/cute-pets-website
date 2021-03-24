cd690da822868e2116ee07babfabb483b72d9433
John Pham <john@highlight.run>
2021-03-24 10:43:19 -0400

    Add starting code

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <h2>About</h2>
+          </Route>
+          <Route path="/users">
+            <h2>Users</h2>
+          </Route>
+          <Route path="/">
+            <h2>Home</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
